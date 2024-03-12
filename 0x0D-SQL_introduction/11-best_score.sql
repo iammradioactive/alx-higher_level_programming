6302103a6099060e9d138598e50948ae6a010208
David <braveukay1@gmail.com>
2024-03-12 16:20:02 -0700

    hot fix

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,4 @@
---list all records with a score >= 10 in a table of the DB
+-- list all records with a score >= 10 in a table of the DB
+-- results display score and name
+-- from DB hbtn_0c_0
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
